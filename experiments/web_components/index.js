5459eb7ba605ad37143b938f463496ed49f93321
Anthony Daves <adaves1@yahoo.com>
2025-01-26 21:07:26 +0530

    Create index.js

---
+++ experiments/web_components/index.js
@@ -0,0 +1,24 @@
+// let canvas = document.getElementById('c');
+// let ctx = canvas.getContext('2d');
+// let width = 600;
+// let height = 500;
+// canvas.width = width;
+// canvas.height = height;
+
+// window.onload = function () {
+
+//   let neon = new Neon(16);
+
+//   neon.createBox(100, 100, 100, 100)
+
+//   function animate() {
+//     ctx.clearRect(0, 0, width, height);
+
+//     neon.update();
+
+//     neon.renderPointIndex();
+
+//     requestAnimationFrame(animate);
+//   }
+//   animate();
+// }
