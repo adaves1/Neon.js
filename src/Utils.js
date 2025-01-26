e5b0c636c3f627b879349cc1bba9cff20a35c6c9
Anthony Daves <adaves1@yahoo.com>
2025-01-26 20:53:31 +0530

    Create Utils.js

---
+++ src/Utils.js
@@ -0,0 +1,33 @@
+let normalizedRandom = function () {
+  return Math.random() * 2 - 1;
+};
+let degreesToRad = function (degrees) {
+  var pi = Math.PI;
+  return degrees * (pi / 180);
+}
+let clamp = function (t, e, i) {
+  return Math.min(Math.max(t, Math.min(e, i)), Math.max(e, i))
+}
+let random = function (rand, min, max) {
+  //one param
+  if (arguments.length === 1) {
+    return Math.random() * arguments[0];
+  } else if (arguments.length == 2) {
+    //min and max
+    max = min;
+    min = rand;
+    rand = Math.random;
+  }
+  if (!min && !max) {
+    return Math.random();
+  } else if (!max) {
+    //if only one is provided, then thats actually the max
+    max = min;
+    return rand() * max;
+  }
+  return rand() * (max - min) + min;
+};
+
+function lerp(a, b, p) {
+  return (b - a) * p + a;
+}
